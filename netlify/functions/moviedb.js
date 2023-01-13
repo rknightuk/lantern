0baa67006b565c766414d236d79ec0e9921dcdd5
Robb Knight <desk@rknight.me>
2023-01-13 23:00:09 +0000

    This?

--- netlify/functions/moviedb.js
+++ netlify/functions/moviedb.js
@@ -3,6 +3,7 @@ const CORS_HEADERS = {
   'Access-Control-Allow-Origin': '*',
   'Access-Control-Allow-Headers':
     'Origin, X-Requested-With, Content-Type, Accept',
+    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
 }
 
 export const handler = async (event) => {
