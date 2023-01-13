c2375bb4ba22ff3b4d6c167b60b663562a0e3999
Robb Knight <desk@rknight.me>
2023-01-13 22:54:17 +0000

    Try this

--- netlify/functions/moviedb.js
+++ netlify/functions/moviedb.js
@@ -15,7 +15,6 @@ export const handler = async (event) => {
   return {
     headers: {
       'Access-Control-Allow-Origin': '*',
-      'Access-Control-Allow-Credentials': true
     },
     statusCode: 200,
     body: JSON.stringify(data.results)
