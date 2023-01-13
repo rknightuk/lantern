4e1925915018edb44946281ec84df4d1f79d20dd
Robb Knight <desk@rknight.me>
2023-01-13 22:55:47 +0000

    Trey this

--- netlify/functions/games.js
+++ netlify/functions/games.js
@@ -1,6 +1,18 @@
 import fetch from 'node-fetch'
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+}
 
 export const handler = async (event) => {
+if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: CORS_HEADERS,
+    }
+  }
+
   const GB_PATH = `http://www.giantbomb.com/api/search?api_key=${process.env.GIANTBOMBKEY}` +
   `&query=${event.queryStringParameters.search}&format=json`
 
@@ -10,7 +22,8 @@ export const handler = async (event) => {
 
   return {
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      ...CORS_HEADERS,
+      'Content-Type': 'application/json',
     },
     statusCode: 200,
     body: JSON.stringify(data.results)
--- netlify/functions/moviedb.js
+++ netlify/functions/moviedb.js
@@ -1,20 +1,29 @@
 import fetch from 'node-fetch'
+const CORS_HEADERS = {
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Headers':
+    'Origin, X-Requested-With, Content-Type, Accept',
+}
 
 export const handler = async (event) => {
+if (event.httpMethod === 'OPTIONS') {
+    return {
+      statusCode: 200,
+      headers: CORS_HEADERS,
+    }
+  }
+
   const TMDB_PATH = `https://api.themoviedb.org/3/search/${event.queryStringParameters.type}?api_key=${process.env.MOVIEDBKEY}` +
   `&query=${event.queryStringParameters.search}`
-
-  console.log(TMDB_PATH)
 
   const response = await fetch(TMDB_PATH)
 
   const data = await response.json()
 
-  console.log(data)
-
   return {
     headers: {
-      'Access-Control-Allow-Origin': '*',
+      ...CORS_HEADERS,
+      'Content-Type': 'application/json',
     },
     statusCode: 200,
     body: JSON.stringify(data.results)
