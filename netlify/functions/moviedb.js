f02234e30cf4a25418f05611fce20d7764c65b8e
Robb Knight <desk@rknight.me>
2023-01-13 22:01:26 +0000

    This is better

--- netlify/functions/moviedb.js
+++ netlify/functions/moviedb.js
@@ -1,12 +1,16 @@
 import fetch from 'node-fetch'
 
 export const handler = async (event) => {
-  const TMDB_PATH = `https://api.themoviedb.org/3/search/tv?api_key=${process.env.MOVIEDBKEY}` +
+  const TMDB_PATH = `https://api.themoviedb.org/3/search/${event.queryStringParameters.type}?api_key=${process.env.MOVIEDBKEY}` +
   `&query=${event.queryStringParameters.search}`
+
+  console.log(TMDB_PATH)
 
   const response = await fetch(TMDB_PATH)
 
   const data = await response.json()
+
+  console.log(data)
 
   return {
     statusCode: 200,
