7ba28dd27f08bc1715a09fd264981dc2c1b0cab0
Robb Knight <desk@rknight.me>
2023-01-14 13:33:27 +0000

    Working

--- netlify/functions/games.js
+++ netlify/functions/games.js
@@ -1,32 +1,26 @@
 import fetch from 'node-fetch'
-const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
-
 export const handler = async (event) => {
-if (event.httpMethod === 'OPTIONS') {
+    const GB_PATH = `http://www.giantbomb.com/api/search?api_key=${process.env.GIANTBOMBKEY}` +
+    `&query=${event.queryStringParameters.search}&format=json`
+
+    const response = await fetch(GB_PATH)
+
+    const data = await response.json()
+
     return {
-      statusCode: 200,
-      headers: CORS_HEADERS,
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        statusCode: 200,
+        body: JSON.stringify(data.results.map(r => {
+            return {
+                id: r.id,
+                title: r.name,
+                year: r.original_release_date ? new Date(r.original_release_date).getFullYear() : null,
+                poster: r.image.original_url,
+                backdrop: r.image.screen_large_url,
+                link: r.site_detail_url,
+            }
+        }))
     }
-  }
-
-  const GB_PATH = `http://www.giantbomb.com/api/search?api_key=${process.env.GIANTBOMBKEY}` +
-  `&query=${event.queryStringParameters.search}&format=json`
-
-  const response = await fetch(GB_PATH)
-
-  const data = await response.json()
-
-  return {
-    headers: {
-      ...CORS_HEADERS,
-      'Content-Type': 'application/json',
-    },
-    statusCode: 200,
-    body: JSON.stringify(data.results)
-  }
 }
--- netlify/functions/moviedb.js
+++ netlify/functions/moviedb.js
@@ -1,32 +1,35 @@
 import fetch from 'node-fetch'
-const CORS_HEADERS = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Headers':
-    'Origin, X-Requested-With, Content-Type, Accept',
-    'Access-Control-Allow-Methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
-}
 
 export const handler = async (event) => {
-if (event.httpMethod === 'OPTIONS') {
+    const type = event.queryStringParameters.type
+    const IMAGEPATH = 'https://image.tmdb.org/t/p/w780'
+    const TMDB_PATH = `http://api.themoviedb.org/3/search/${type}?api_key=${process.env.MOVIEDBKEY}` +
+    `&query=${event.queryStringParameters.search}`
+
+    const response = await fetch(TMDB_PATH)
+    const data = await response.json()
+
     return {
-      statusCode: 200,
-      headers: CORS_HEADERS,
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        statusCode: 200,
+        body: JSON.stringify(data.results.map(r => {
+            let title = r.title
+            let year = r.release_date ? new Date(r.release_date).getFullYear() : null
+            if (type === 'tv')
+            {
+                title = r.name
+                year = r.first_air_date ? new Date(r.first_air_date).getFullYear() : null
+            }
+            return {
+                id: r.id,
+                title,
+                year,
+                poster: r.poster_path ? `${IMAGEPATH}${r.poster_path}` : null,
+                backdrop: r.backdrop_path ? `${IMAGEPATH}${r.backdrop_path}` : null,
+                link: `https://www.themoviedb.org/${type}/${r.id}`,
+            }
+        }))
     }
-  }
-
-  const TMDB_PATH = `http://api.themoviedb.org/3/search/${event.queryStringParameters.type}?api_key=${process.env.MOVIEDBKEY}` +
-  `&query=${event.queryStringParameters.search}`
-
-  const response = await fetch(TMDB_PATH)
-
-  const data = await response.json()
-
-  return {
-    headers: {
-      ...CORS_HEADERS,
-      'Content-Type': 'application/json',
-    },
-    statusCode: 200,
-    body: JSON.stringify(data.results)
-  }
 }
